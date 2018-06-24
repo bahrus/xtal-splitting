57948e4adde1e13959c0ed156694882024c1393c
bahrus <anderson.bruce.b@gmail.com>
2018-06-23 22:40:48 -0400

    ...

--- xtal-split.js
+++ xtal-split.js
@@ -1,33 +1,28 @@
-import {XtallatX} from 'xtal-latx/xtal-latx.js';
+import { XtallatX } from 'xtal-latx/xtal-latx.js';
 const search = 'search';
 const text_content = 'text-content';
 export class XtalSplit extends XtallatX(HTMLElement) {
-    static get is(){return 'xtal-split';}
-    static get observedAttributes(){
+    static get is() { return 'xtal-split'; }
+    static get observedAttributes() {
         return super.observedAttributes.concat([search, text_content]);
     }
-    attributeChangedCallback(name: string, oldVal: string, newVal: string){
-        switch(name){
+    attributeChangedCallback(name, oldVal, newVal) {
+        switch (name) {
             case search:
                 this._search = newVal;
                 break;
             case text_content:
                 this._textContent = newVal ? this.strip(newVal) : '';
-                break;            
+                break;
         }
         this.onPropsChange();
     }
-    _originalText: string;
-
-    _search: string;
     get search() {
         return this._search;
     }
     set search(val) {
         this.attr(search, val);
     }
-
-    _textContent: string;
     get textContent() {
         return this._textContent;
     }
@@ -35,24 +30,22 @@ export class XtalSplit extends XtallatX(HTMLElement) {
         //this._textContent = this.strip(val);
         this.attr(text_content, val);
     }
-
     strip(html) {
         var tmp = document.createElement("DIV");
         tmp.innerHTML = html;
         return tmp.textContent || tmp.innerText || "";
     }
-
-
-    _connected : boolean;
-    connectedCallback(){
+    connectedCallback() {
         this._connected = true;
         this._upgradeProperties(['search', 'textContent']);
     }
     onPropsChange() {
-        if (this._textContent) return;
+        if (this._textContent)
+            return;
         if (!this._search) {
             this.innerText = this._textContent;
-        } else {
+        }
+        else {
             const split = this._textContent.split(new RegExp(this._search, 'i'));
             const tokenCount = split.length;
             const len = this._search.length;
@@ -60,16 +53,16 @@ export class XtalSplit extends XtallatX(HTMLElement) {
             let text = '';
             //console.log(split);
             split.forEach((token, idx) => {
-
                 iPos += token.length;
                 text += token;
-                if (idx < tokenCount) text += "<span class='match'>" + this._textContent.substr(iPos, len) + "</span>";
+                if (idx < tokenCount)
+                    text += "<span class='match'>" + this._textContent.substr(iPos, len) + "</span>";
                 iPos += len;
-            })
+            });
             this.innerHTML = text;
         }
-
     }
 }
-
-if(customElements.get(XtalSplit.is)) customElements.define(XtalSplit.is, XtalSplit);
+if (customElements.get(XtalSplit.is))
+    customElements.define(XtalSplit.is, XtalSplit);
+//# sourceMappingURL=xtal-split.js.map
